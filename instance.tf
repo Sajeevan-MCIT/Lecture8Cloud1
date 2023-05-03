10a62169b5555c583692c39e6c34f984b37b31b9
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-05-03 19:40:40 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -3,14 +3,14 @@ provider "aws" {
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_SECRET_KEY
 }
-resource "aws_instance" "web" {
+resource "aws_instance" "web-1" {
   ami ="ami-0889a44b331db0194"
   instance_type="t3.micro"
   tags={
     Name="Sajeevan-Arun-2"
   } 
 } 
-resource "aws_instance" "web" {
+resource "aws_instance" "web-2" {
   ami ="ami-0889a44b331db0194"
   instance_type="t3.micro"
   tags={
