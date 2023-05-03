223762171db254ae2eb2263072345062810ddf4d
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-05-03 18:57:00 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key=var.AWS_SECRET_KEY
 }
 resource "aws_instance" "web" {
-  ami ="ami-02dc8ad50da58fffd"
+  ami ="ami-0889a44b331db0194"
   instance_type="t3.micro"
   tags={
     Name="HelloWorld"
