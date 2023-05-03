260df973c92698d9944f23bccd7444c39c8e9fc3
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-05-03 19:30:34 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -7,6 +7,11 @@ resource "aws_instance" "web" {
   ami ="ami-0889a44b331db0194"
   instance_type="t3.micro"
   tags={
-    Name="HelloWorld"
+    Name="Sajeevan-Arun-2"
   } 
+resource "aws_instance" "web" {
+  ami ="ami-0889a44b331db0194"
+  instance_type="t3.micro"
+  tags={
+    Name="Sajeevan-Arun-1"  
 }
